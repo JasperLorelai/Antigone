b8fe687824835c55d917cedf5372ecaf869071dd
JasperLorelai <wolf.haci12@gmail.com>
2025-07-25 02:21:24 +0200

    refactor: Remap duplicate NMS versions

--- nms/shared/src/main/java/eu/jasperlorelai/antigone/nms/shared/util/Util.java
+++ nms/shared/src/main/java/eu/jasperlorelai/antigone/nms/shared/util/Util.java
@@ -1,17 +1,16 @@
 package eu.jasperlorelai.antigone.nms.shared.util;
 
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 import net.minecraft.server.Bootstrap;
 
 public class Util {
 
-	private static final Map<String, String> VERSION_REMAP = Map.of(
-			"1.21.1", "1.21"
-	);
+	private static final Map<String, String> VERSION_REMAP = new LinkedHashMap<>() {{
+		put("1.21.1", "1.21");
+		put("1.21.7", "1.21.6");
+		put("1.21.8", "1.21.6");
+	}};
 
 	private static final Map<String, List<String>> REVERSE_VERSION_MAP = new HashMap<>();
 	static {
